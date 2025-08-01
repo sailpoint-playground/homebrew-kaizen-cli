f76679f2b54bd04b1c7122061beab97e3c85b545
victorbiga <victor.biga@sailpoint.com>
2025-08-01 14:01:19 +0100

    add cask

---
+++ kaizen.rb
@@ -0,0 +1,16 @@
+cask "kaizen" do
+  version "0.1.0"
+  sha256 "93d0f64fc93d8580e96b405fb1905d43f5e98788c6c99df3976e6350637a3d32"
+
+  url "https://github.com/sailpoint-playground/kaizen-cli/raw/refs/heads/main/kaizen", verified: "github.com/sailpoint-playground/"
+  name "kaizen"
+  desc "A powerful CLI for streamlined CI/CD operations."
+  homepage "https://github.com/sailpoint-playground/kaizen-cli"
+
+  binary "kaizen"
+
+  livecheck do
+    url :homepage
+    regex(/Kaizen\s+v?(\d+(?:\.\d+)+)/i)
+  end
+end
