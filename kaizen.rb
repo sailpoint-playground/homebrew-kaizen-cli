c0952a4d09deb3eba3241cd605067eee3ac3437e
victor-biga-sp <victor.biga@sailpoint.com>
2025-08-15 17:04:41 +0100

    Update kaizen.rb

--- kaizen.rb
+++ kaizen.rb
@@ -1,16 +1,14 @@
-cask "kaizen-cli" do
-  version "0.1.0"
-  sha256 "93d0f64fc93d8580e96b405fb1905d43f5e98788c6c99df3976e6350637a3d32"
-
-  url "https://github.com/sailpoint-playground/kaizen-cli/raw/refs/heads/main/kaizen", verified: "github.com/sailpoint-playground/"
-  name "kaizen"
+class KaizenCli < Formula
   desc "A powerful CLI for streamlined CI/CD operations."
   homepage "https://github.com/sailpoint-playground/kaizen-cli"
 
-  binary "kaizen"
+  url "https://github.com/sailpoint-playground/kaizen-cli/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "93d0f64fc93d8580e96b405fb1905d43f5e98788c6c99df3976e6350637a3d32"
 
-  livecheck do
-    url :homepage
-    regex(/Kaizen\s+v?(\d+(?:\.\d+)+)/i)
+  def install
+    # This is a placeholder. You'll need to specify
+    # the correct commands to build and install your
+    # binary, like `make install` or `bin.install "kaizen"`.
+    bin.install "kaizen"
   end
 end
