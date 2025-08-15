769693e5056939a051db8ce5b18636ef784689cd
victor-biga-sp <victor.biga@sailpoint.com>
2025-08-15 17:05:53 +0100

    Update kaizen.rb

--- kaizen.rb
+++ kaizen.rb
@@ -1,8 +1,8 @@
 class KaizenCli < Formula
   desc "A powerful CLI for streamlined CI/CD operations."
-  homepage "https://github.com/sailpoint-playground/kaizen-cli"
+  homepage "https://github.com/sailpoint-playground/homebrew-kaizen-cli"
 
-  url "https://github.com/sailpoint-playground/kaizen-cli/archive/refs/tags/v0.1.0.tar.gz"
+  url "https://github.com/sailpoint-playground/homebrew--kaizen-cli/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "93d0f64fc93d8580e96b405fb1905d43f5e98788c6c99df3976e6350637a3d32"
 
   def install
